876989d703c053d8ce305f367bd15bca6169c5f7
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2021-07-11 19:08:51 +0545

    Updated !

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,4 +1,4 @@
-from _ast import Lambda
+
 from tkinter import *
 from tkinter.font import *
 
@@ -29,7 +29,7 @@ def button_click_equal():
 
 
 def button_click_delete():  # need to be changed
-    screenEntry.delete(0, END)
+    pass
 
 
 def button_click_allClear():
@@ -37,6 +37,12 @@ def button_click_allClear():
 
 
 def button_click_equal():  # need to be changed
+    screenEntry.delete(0, END)
+
+def button_add():
+    first_number = screenEntry.get()
+    global f_num
+    f_num = int(first_number)
     screenEntry.delete(0, END)
 # Creating digit buttons and entry fields
 
@@ -89,7 +95,7 @@ button_click_7['font'] = myFont
 button_click_8 = Button(calculator, text="8", padx=25, pady=8, highlightbackground="#000000", fg="#FFFFFF", command=lambda: button_click_number(8))
 button_click_8.grid(row=4, column=1)
 button_click_8['font'] = myFont
-button_click_9 = Button(calculator, text="9", padx=25, pady=8, highlightbackground="#000000", fg="#FFFFFF", command=lambda: button_click_number(9))
+button_click_9 = Button(calculator, text="9", padx=25, pady=8, highlightbackground="black", fg="#FFFFFF", command=lambda: button_click_number(9))
 button_click_9.grid(row=4, column=2)
 button_click_9['font'] = myFont
 
@@ -111,14 +117,14 @@ button_click_multiple = Button(calculator, text="x", padx=25, pady=8, highlightb
 button_click_multiple.grid(row=4, column=4)
 button_click_multiple['font'] = myFont
 
-button_click_plus = Button(calculator, text="+", padx=25, pady=8, highlightbackground="green", fg="#000000", command=lambda: button_click_number('+'))
+button_click_plus = Button(calculator, text="+", padx=25, pady=8, highlightbackground="#008000", fg="#000000", command=lambda: button_click_number('+'))
 button_click_plus.grid(row=6, column=3)
 button_click_plus['font'] = myFont
 button_click_minus = Button(calculator, text="-", padx=25, pady=8, highlightbackground="green", fg="#000000", command=lambda: button_click_number('-'))
 button_click_minus.grid(row=6, column=4)
 button_click_minus['font'] = myFont
 
-button_click_equally = Button(calculator, text="=", padx=68, pady=40, highlightbackground="blue", fg="#000000", command=button_click_equal)
+button_click_equally = Button(calculator, text="=", padx=68, pady=40, highlightbackground="blue", fg="black", command=button_click_equal)
 button_click_equally.grid(row=8, column=3, rowspan=2, columnspan=2)
 button_click_equally['font'] = myFont
-calculator.mainloop()
+mainloop()
